a3195c59d50d683ed715f7a471112bb2445ba852
Fred Mesnard <frederic.mesnard@gmail.com>
2026-02-25 22:06:46 +0400

    Add predicate to check if sqrt(2) is rational

--- sqrt2.pl
+++ sqrt2.pl
@@ -4,5 +4,11 @@ even(s(s(X))) :- even(X).
 odd(s(0)).
 odd(s(s(X))) :- odd(X).
 
-
+sqrt_two_is_rational :- 
+	nat(S),
+	plus(P,Q,S),
+	gcd(P,Q,s(0)),
+	times(P,P,P2),
+	times(Q,Q,Q2),
+	plus(Q2,Q2,P2).
 
